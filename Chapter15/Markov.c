daf664f8ed4fc90c7b652d8d4bf8f259018c4a5a
binisforbinary <binisforbinary@gmail.com>
2015-01-08 23:26:13 +0800

    finish Markov.c

--- Chapter15/Markov.c
+++ Chapter15/Markov.c
@@ -7,20 +7,23 @@
 
 char s[5000000];
 char *a[1000000];
-unsigned int num = 0;
+int num = 0;
 
 //Markov's order
-const unsigned int K = 1;
+const int K = 1;
 
-int compare(char **p, char **q)
+int wordncmp(char *p, char *q)
 {
 	int n = K;
-	char *p1 = *p;
-	char *q1 = *q;
-	for(; *p1 == *q1; p1++, q1++)
-		if(*p1 == 0 && --n ==0)
+	for(; *p == *q; p++, q++)
+		if(*p == 0 && --n == 0)
 			return 0;
-	return *p1-*q1;
+	return *p - *q;
+}
+
+int sortcmp(char **p, char **q)
+{	
+	return wordncmp(*p, *q);
 }
 
 // generate random integer in [left, right]
@@ -31,46 +34,73 @@ int random_int(int left, int right)
 	return (int)d2 + left;
 }
 
+// skip to the next n words from p
+char* skip(char *p, int n)
+{
+	for(; *p || --n > 0; p++)
+		;
+	return ++p;
+}
+
 int main()
 {
 	a[0] = s;
 	while(scanf("%s", a[num]) != EOF) {
-		a[num+1] = &s[strlen(a[num])+1];
+		a[num+1] = a[num] + strlen(a[num]) + 1;
 		num++;
 	}
 
-	int i = 0;
-	for(; i < K; i++)
+	int i, j;
+	for(i = 0; i < K; i++)
 		a[num][i] = 0;
 
+	//print the first K words
+	for(i = 0; i < K; i++)
+		printf("%s ", a[i]);
+
 	// sort the suffix array
-	qsort(a, num, sizeof(char*), compare);
+	qsort(a, num, sizeof(char*), sortcmp);
 
 	//set random seed
 	srand((int)time(NULL));
 
-	if(num == 0) return;
-
-	//print the start word
-	unsigned int index = random_int(0, num-1);
-	char *str = a[index];
-	printf("%s", str);
-
-	// binary search the 'str'
-	// this version of binary search will find the FIRST candidate if it exists
-	int i = -1, j = num;
-	while(i+1 != j) {
-		int mid = (j-i)/2;
-		if(compare(&str, &a[mid]) > 0) i = mid;
-		else j = mid; 
-	}
-
-	for(i = 0; compare(&str, a[j+i]) == 0; i++)
-		if(rand() % (i+1) == 0)
-			str = a[j+i];
-
-	// encounter the last word
-	if(*(str+strlen(str)+1) == 0)
+	if(num < K) { 
+		printf("total words less than K--%d\n", K);
 		return;
+	}
+		
+	char *start_ptr = s, *p;
 	
+	// generate T words most
+	int T = 100;
+	while(--T > 0) {
+		// binary search the 'start_ptr'
+		// this version of binary search will find the FIRST candidate if it exists
+		i = -1;
+		j = num;
+		while(i+1 != j) {
+			int mid = (j+i)/2;
+			if(wordncmp(start_ptr, a[mid]) > 0) i = mid;
+			else j = mid; 
+		}
+		for(i = 0; wordncmp(start_ptr, a[j+i]) == 0; i++)
+			if(rand() % (i+1) == 0)
+				p = a[j+i];
+
+		// encounter the last word
+		if(strlen(skip(p, K)) == 0)
+			return;
+
+		// print the candidate word
+		printf("%s ", skip(p, K));
+
+		// set 'start_ptr'
+		start_ptr = skip(p, 1);
+	}
 }
+
+
+
+
+
+
